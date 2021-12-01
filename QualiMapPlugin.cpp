4a4f08634518b6b10f51cca2235853468b007665
Trevor Cickovski <tcickovs@delphi.cs.fiu.edu>
2021-12-01 15:06:12 -0500

    Copy TXT file as well

--- QualiMapPlugin.cpp
+++ QualiMapPlugin.cpp
@@ -15,7 +15,7 @@ void QualiMapPlugin::output(std::string file) {
  // spades.py -o 02_QualiMap -1 $1 -2 $2 --careful
  // assembly-stats -t ./All_assemblies/*.fasta > ./All_assemblies/00_Metrics_all_assemblies.txt
  // qualimap bamqc -bam assembly_map_long-reads.bam -outfile Qualimap_map_long.pdf
-	std::string command = "qualimap bamqc -bam "+inputfile+" -outfile "+file.substr(file.find_last_of('/')+1)+"; cp "+std::string(PluginManager::prefix())+"/*stats/*.pdf "+std::string(PluginManager::prefix());
+	std::string command = "qualimap bamqc -bam "+inputfile+" -outfile "+file.substr(file.find_last_of('/')+1)+"; cp "+std::string(PluginManager::prefix())+"/*stats/* "+std::string(PluginManager::prefix());
  std::cout << command << std::endl;
 
  system(command.c_str());
